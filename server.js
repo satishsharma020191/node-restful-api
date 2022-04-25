224cfbfd68bd1f763038cb0e09feef1c78df412c
satishsharma020191 <satishsharma020191@gmail.com>
2022-04-25 12:05:03 +0530

    making port dynamic for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const database = require("./db/connection");
 
 // Configuring body parser middleware
